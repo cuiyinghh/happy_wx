c59de9e416f72d41b35048c4e2978cb7654f67af
cuiying <971345565@qq.com>
2018-03-12 19:52:14 +0800

    happy start

---
+++ components/loading/loading.js
@@ -0,0 +1,32 @@
+
+Component({
+  /**
+   * 父组件传递的属性
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+  
+  //在组件实例进入页面节点树时执行，注意此时不能调用 setData
+  created: function() {
+
+  },
+
+  //在组件布局完成后执行，此时可以获取dom
+  ready: function (obj) {
+
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
